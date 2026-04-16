4dbc30031df469e016ff95eebf492e0a0816f0c3
gjsb46 <schneider-batemang@uwstout.edu>
2026-04-15 21:25:35 -0500

    Update include.lua

--- include.lua
+++ include.lua
@@ -1,3 +1,15 @@
+function RawBlock(el)
+  local filename = el.text:match("^!include (.+)$")
+  if filename then
+    local f = io.open(filename, "r")
+    if f then
+      local content = f:read("*all")
+      f:close()
+      return pandoc.read(content).blocks
+    end
+  end
+end
+
 function Para(el)
   local text = pandoc.utils.stringify(el)
   local filename = text:match("^!include (.+)$")
@@ -10,3 +22,6 @@ function Para(el)
     end
   end
 end
+Replace your current include.lua with this and commit. It now handles the !include syntax whether Pandoc treats it as a paragraph or a raw block.
+
+
